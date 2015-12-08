386a7537264b47abacaed75b8312247b06d9fcb5
ROYER-ADNOT Remy <remy.royeradnot.pro@gmail.com>
2015-12-08 19:22:57 +0100

    initial commit : setting up environment

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name yopaNotesApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the yopaNotesApp
+ */
+angular.module('yopaNotesApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
